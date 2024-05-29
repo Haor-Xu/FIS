539270fe7d44ba04941ead9754aa3eaba1627c66
Haoran Xu <150667446+Haor-Xu@users.noreply.github.com>
2024-05-29 15:11:46 +0200

    P1.c aktualisieren

--- P1.c
+++ P1.c
@@ -8,8 +8,29 @@ void MSRProduct (int *MSR_JM, double *MSR_VM, double *Vec, int n, double *Result
    }
    
 }
+#include <stdio.h>
+
 int main() {
-    // 打印 "Hello, World!" 到控制台
-    printf("Hello, World!\n");
-    return 0; // 程序执行成功返回 0
-} 
+    FILE *file = fopen("data.txt", "r");  // 打开文件，假设文件名为data.txt
+    if (file == NULL) {
+        printf("无法打开文件\n");
+        return 1;
+    }
+
+    double array1[1000];  // 假设最多1000行数据
+    double array2[1000];
+    int count = 0;
+
+    while (fscanf(file, "%lf %lf", &array1[count], &array2[count]) == 2) {
+        count++;  // 读取每行的两个浮点数，存储到两个数组中
+    }
+
+    fclose(file);  // 关闭文件
+
+    // 输出读取的数据，验证结果
+    for (int i = 0; i < count; i++) {
+        printf("array1[%d] = %f, array2[%d] = %f\n", i, array1[i], i, array2[i]);
+    }
+
+    return 0;
+}
